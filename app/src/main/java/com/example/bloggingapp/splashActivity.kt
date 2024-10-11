1c03653e8afec28faab6b6a30405d71d98473ac8
Ali Waqas <aliwaqas.techstudent.cs@gmail.com>
2024-10-11 10:46:43 -0700

    first commit

---
+++ app/src/main/java/com/example/bloggingapp/splashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.bloggingapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+        Handler(mainLooper).postDelayed({
+
+
+        } , 5000)
+        startActivity(Intent( this,MainActivity ::class.java))
+        finish();
+
+
+
+}
+}
+
+
+
+
